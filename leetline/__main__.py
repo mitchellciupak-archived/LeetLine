4d21dc1aa062588256036fccef3893fa694234c1
mitchellciupak <ciupak@purdue.edu>
2020-06-12 15:24:42 -0500

    Doc Update

--- leetline/__main__.py
+++ leetline/__main__.py
@@ -1,6 +1,9 @@
 import sys
-from .classmodule import MyClass
-from .funcmodule import my_function
+#from .classmodule import MyClass
+#from .funcmodule import my_function
+
+## https://code.visualstudio.com/docs/python/python-tutorial
+## https://trstringer.com/easy-and-nice-python-cli/
 
 def main():
     print('in main')
@@ -9,10 +12,12 @@ def main():
     for arg in args:
         print('passed argument :: {}'.format(arg))
 
-    my_function('hello world')
+    #my_function('hello world')
 
-    my_object = MyClass('Thomas')
-    my_object.say_name()
+    #my_object = MyClass('Thomas')
+    #my_object.say_name()
 
 if __name__ == '__main__':
     main()
+
+    # & C:/Users/mjcre/AppData/Local/Programs/Python/Python38-32/python.exe c:/Users/mjcre/Documents/LeetLine/leetline/__main__.py test
