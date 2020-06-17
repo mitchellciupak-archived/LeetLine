1142282d5f466c07ab65e1fefb05271dbf7839ff
mitchellciupak <ciupak@purdue.edu>
2020-06-16 19:40:34 -0500

    Problem Class Init, Complexity Code Added

---
+++ leetline/problem.py
@@ -0,0 +1,16 @@
+#CreatedBy: Mitchell Ciupak
+#Created On: 20200616
+
+# Import
+import os
+import sys
+
+class Problem: 
+
+    def __init__(self):
+        self.Name = input("Please enter the problem name: ")
+        #self.dateTimeCreated = Program.getDateTime()
+  
+    # a method for printing data members 
+    def print_Geek(self): 
+        print(self.name) 
