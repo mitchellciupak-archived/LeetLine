8cc33cedeaf3334be01e3ffe78d0b626bf283714
mitchellciupak <ciupak@purdue.edu>
2020-06-16 20:28:21 -0500

    Problem.createNewProblem Init

--- leetline/problem.py
+++ leetline/problem.py
@@ -9,8 +9,8 @@ import sys
 class Problem: 
 
     def __init__(self):
-        self.getName()
-        self.source = input("Please enter a link or description of the source: ") 
+        self.name = input("1. Please enter the problem name: ")
+        self.source = input("2. Please enter a link or description of the source: ") 
         #self.dateTimeCreated = Program.getDateTime()
         self.createNewProblem()
 
@@ -18,21 +18,20 @@ class Problem:
         #Trigger Deconstructor by: del obj
         #Pickle it! 
         print("Self Destructing .....")
-  
-    # Method Gets Name By Input and Creates File Names
-    def getName(self):
-        self.name = input("Please enter the problem name: ")
-        self.filename = ''.join(word.title() for word in self.name.split(' '))
-        print(self.filename)
-
 
     # Method creates the template to implament and slove a problem
     def createNewProblem(self): 
-        print(self.name) 
+        self.foldername = Program.getPrefDirectory + "\" + self.name
+        self.filename = self.foldername + ''.join(word.title() for word in self.name.split(' '))
+
+        #Create Project File
+        if(Program.getPrefLanguage == 'Python') {
+            fptr = open(self.filename + ".py","w+")
+            f.write("Write in basic template with descripion and everything")
+            fptr.close()
+        }
 
     
-
-
-print("test")
+#TODO Debug
 obj = Problem()
 del obj
