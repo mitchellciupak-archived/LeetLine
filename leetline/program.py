61474bf11b1585a43b05b40d7e3fa6bca9126687
mitchellciupak <ciupak@purdue.edu>
2020-06-17 22:09:11 -0500

    Simplified Program Class

--- leetline/program.py
+++ leetline/program.py
@@ -22,47 +22,35 @@ class Program:
 
     # Pull Current DateTime (yyyyMMdd_hhmmssss)
     def getDateTime(self): 
-        print ("Under Construction")
-        return "17760704_00000000"
+        #TODO Have not decided best format yet
+        print ("Date Time Under Construction")
+        self.dateTime = "17760704_00000000"
+        return self.dateTime
 
+    # Prompts user to update directory information
     def getPrefDirectory(self):
-        try:
-            os.path.isdir(self.dir)
-            return self.dir
-        except:
+        self.dir = input("Please enter your folder path: ") 
+        while not (os.path.isdir(self.dir)):
+            print("This folder was not found. Please Create and Try Again")
             self.dir = input("Please enter your folder path: ") 
-            while not (os.path.isdir(self.dir)):
-                print("This folder was not found. Please Create and Try Again")
-                self.dir = input("Please enter your folder path: ") 
-            return self.dir
+        return self.dir
 
     def getPrefEditor(self):
         self.editor = "VS Code"
+        #TODO Havent Decided best way to open the developmet environemnt
+        print ("GetPrefEditor is Under Construction")
         return self.editor
 
     def getPrefLanguage(self):
-        try:
-            return self.lang
-        except:
+        self.lang = input("Please enter your programming language: ") 
+        while not(self.lang == "Python" or self.lang == "C/C++"):
+            print("I'm sorry, we are currently only supporting 'Python' or 'C/C++.'")
             self.lang = input("Please enter your programming language: ") 
-            while not(self.lang == "Python" or self.lang == "C/C++"):
-                print("I'm sorry, we are currently only supporting 'Python' or 'C/C++.'")
-                self.lang = input("Please enter your programming language: ") 
-            return self.lang
+        return self.lang
 
     def getPrefName(self):
-        try:
-            return self.author
-        except:
-            self.author = input("Please enter your name/alias: ")
+        self.author = input("Please enter your name/alias: ")
         return self.author
-
-    def changeUser(self):
-        del self.author
-        del self.lang
-        del self.dir
-        del self.editor
-        self.newUser()
 
     def newUser(self):
         name = self.getPrefName()
@@ -74,6 +62,6 @@ class Program:
         print("Your name is " + name + ", who speaks in the toung of " + lang + ", in the land of " + folder + " and you use " + editor + " as your wepon of choice against evil!\n")
         confirmation = input("Press enter to continue or type 'I have a small penis' to change your settings: ")
         if(confirmation != ''):
-            self.changeUser()
+            self.newUser()
         #TODO Production
         #print("Great! Remember you can chage your settings anytime by using the command 'leetline.exe changeUser'")
